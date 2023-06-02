e6e9ed1aa42d1eade7085e39fb28e82fa23c55d2
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-06-02 21:29:13 +0100

    backend initial setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose'
+
+const app = express();
+
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb+srv://danielakinyemi:p4QYTQm50BToHW4S@jollylyrecipe.1yycsej.mongodb.net/jollylyrecipe?retryWrites=true&w=majority")
+
+app.listen(3001, ()=> console.log("SERVER STARTED!"))
